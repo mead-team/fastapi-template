f6ebd030ab04803bd41d9d5ffb992727ff6c7b37
jewon119 <jewon119@gmail.com>
2024-02-19 14:48:17 +0900

    add: ProcessTime 미들웨어 추가

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.metadata import swagger_metadata
+from app.core.middleware import ProcessTimeMiddleware
 from app.core.setting import settings
 
 
@@ -24,6 +25,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(ProcessTimeMiddleware)
 
 
 @app.get("/api-health-check")
